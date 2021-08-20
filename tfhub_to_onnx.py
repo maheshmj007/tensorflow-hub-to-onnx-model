dfdd140c283978f75869ad211a8aa7ac2814f1a0
maheshmj007 <maheshmj007@gmail.com>
2021-08-20 21:19:23 +0530

    initial commit

---
+++ tfhub_to_onnx.py
@@ -0,0 +1,16 @@
+import tensorflow_hub as hub
+import tensorflow as tf
+import onnxmltools
+
+# tf.compat.v1.disable_eager_execution()
+# sess = tf.compat.v1.Session()
+IMAGE_SIZE = (224,224)
+model = tf.keras.Sequential([
+    tf.keras.layers.InputLayer(input_shape=IMAGE_SIZE + (3,)),
+    hub.KerasLayer("https://tfhub.dev/google/remote_sensing/bigearthnet-resnet50/1",tags='train'),
+    ])
+model.build((None,)+IMAGE_SIZE+(3,))
+
+
+onnx_model = onnxmltools.convert_keras(model, target_opset=6)
+keras2onnx.save_model(onnx_model, 'model.onnx')
